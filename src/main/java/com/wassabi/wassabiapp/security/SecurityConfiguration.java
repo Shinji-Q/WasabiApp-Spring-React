de29b6702f52810c4eddc769514f13e9906fafe2
Yuri Matos <yurimatos40@gmail.com>
2022-11-19 19:17:24 -0300

    Mudanças no security

--- src/main/java/com/wassabi/wassabiapp/security/SecurityConfiguration.java
+++ src/main/java/com/wassabi/wassabiapp/security/SecurityConfiguration.java
@@ -1,14 +1,18 @@
 package com.wassabi.wassabiapp.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @EnableWebSecurity
@@ -43,6 +47,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 
     }
 
+    @Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+    
     @Bean
     public PasswordEncoder getPasswordEncoder() { 
         return NoOpPasswordEncoder.getInstance();
