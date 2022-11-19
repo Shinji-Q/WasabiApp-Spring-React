16973e8c3170e172a287d2b398d4d133d075f1d2
Alfredo Gabriel <amgabriel1@hotmail.com>
2022-11-19 05:57:44 -0300

    teste

--- src/main/java/com/wassabi/wassabiapp/controller/ProdutoController.java
+++ src/main/java/com/wassabi/wassabiapp/controller/ProdutoController.java
@@ -1,6 +1,7 @@
 package com.wassabi.wassabiapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
+    @CrossOrigin(origins = "http://localhost:80")
     @GetMapping("/produto")
     public Iterable<Produto> getAllProduto(){
         return produtoService.getAllProduto();
